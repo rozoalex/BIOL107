5f95ff5e30e9d1f2926e300bbb5cf7e671217d99
airzon <acchiu8@gmail.com>
2015-11-07 16:29:27 -0500

    Update analyze_mouse_arm.m

--- TP1/analyze_mouse_arm.m
+++ TP1/analyze_mouse_arm.m
@@ -1,21 +1,34 @@
-function [a,b,c,d] = analyze_mouse_arm(directoryName)
+function [a,b,c,d] = analyze_mouse_arm(folder)
+% analyze_mouse_arm - Analyze folder of data
+%  [a,b,c,d] = analyze_mouse_arm(foldername)
+%
+%  Goes through all subdirectories of foldername and looks for the file
+%  'mouse_arm_data.txt'.
+%
+%  If it finds one, then it performs a fit
+%  using fit_arm_nerve.m.  The A, B, C, D
+%  of that function are returned.
+%
+%
 
-  subdirs = dir(directoryName);
-  fileCount = length(subdirs);
-  % initialize arrays to store parameters
-  a = NaN(fileCount, 1);
-  b = NaN(fileCount, 1);
-  c = NaN(fileCount, 1);
-  d = NaN(fileCount, 1);
-  for i = 1:fileCount
-    % load data from file
-    if subdirs(i).isdir && ~strcmp(subdirs(i).name, '.') && ~strcmp(subdirs(i).name, '..')
-      filename = [directoryName filesep subdirs(i).name filesep 'mouse_arm_data.txt'];
-      [a_, b_, c_, d_] = fit_arm_nerve(filename);
-        a(i) = a_;
-        b(i) = b_;
-        c(i) = c_;
-        d(i) = d_;
-    end
-  end
-end
+subdirs = dir(folder);
+a = []; b = []; c = []; d = [];
+
+for i=1:length(subdirs), %repeat until through looking through all subdirectories
+    %Once in subdirectory, will look for anything named
+    %'mouse_arm_data.txt'
+    if subdirs(i).isdir&~strcmp(subdirs(i).name,'.')&~strcmp(subdirs(i).name,'..'),
+        filename = [folder filesep subdirs(i).name filesep 'mouse_arm_data.txt'];
+        %If 'mouse_arm_data.txt' is found, it will perform fit_arm_nerve.m
+        %and output best fit parameters to a, b, c, and d.
+        if exist(filename),
+            disp(['Analyzing file ' filename '.']);
+            [a_,b_,c_,d_] = fit_arm_nerve(filename);
+            a(end+1) = a_;
+            b(end+1) = b_;
+            c(end+1) = c_;
+            d(end+1) = d_;
+            %then repeat for other subdirectories
+        end;
+    end;
+end;
